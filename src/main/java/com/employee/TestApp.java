32461bd38cec793650bd217cb2c19f2b0c59bc43
Gaurav Kshirsagar <gauravmanojkshirsagar333@gmail.com>
2026-02-08 16:06:17 +0530

    Refactored project structure and cleaned code

--- src/main/java/com/employee/TestApp.java
+++ src/main/java/com/employee/TestApp.java
@@ -13,26 +13,26 @@ public class TestApp {
 	public static void main(String[] args) {
 		EmployeeController ec = new EmployeeController();
 
-//		Department it = new Department("IT");
-//
-//		Employee e1 = new Employee("Gaurav", 50000, it);
-//		Employee e2 = new Employee("Rahul", 60000, it);
-//
-//		EmployeeProfile profile1 = new EmployeeProfile("Pune", "98323343", "14/4/2004", e1);
-//
-//		EmployeeProfile profile2 = new EmployeeProfile("Pune", "7447321292", "19/3/2006", e2);
-//		e1.setProfile(profile1);
-//		e2.setProfile(profile2);
-//
-//		Project p1 = new Project("Hibernate Project");
-//
-//		e1.setProjects(Arrays.asList(p1));
-//		e2.setProjects(Arrays.asList(p1));
-//		p1.setEmployees(Arrays.asList(e1, e2));
-//
-//		ec.insertEmployee(e1);
-//		ec.insertEmployee(e2);
-		
+		Department it = new Department("IT");
+
+		Employee e1 = new Employee("Gaurav", 50000, it);
+		Employee e2 = new Employee("Rahul", 60000, it);
+
+		EmployeeProfile profile1 = new EmployeeProfile("Pune", "9832300343", "14/4/2004", e1);
+		EmployeeProfile profile2 = new EmployeeProfile("Pune", "7447300292", "19/3/2006", e2);
+
+		e1.setProfile(profile1);
+		e2.setProfile(profile2);
+
+		Project p1 = new Project("Hibernate Project");
+
+		e1.setProjects(Arrays.asList(p1));
+		e2.setProjects(Arrays.asList(p1));
+		p1.setEmployees(Arrays.asList(e1, e2));
+
+		ec.insertEmployee(e1);
+		ec.insertEmployee(e2);
+
 		ec.showAllEmployees();
 	}
 }
